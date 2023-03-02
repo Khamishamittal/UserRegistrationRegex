1d3c5f57f744741c98bde1c56228c7b5cf9d018a
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-03-02 20:01:35 +0530

    Create a Valid First Name Using Regex

--- Program.cs
+++ Program.cs
@@ -6,6 +6,25 @@
         {
 
             Console.WriteLine("Welcome to User Registration Regex Program");
+
+            ValidFirstName firstname = new ValidFirstName();
+
+            Console.Write("Enter Your First Name : ");
+
+            string FirstName = Console.ReadLine();
+
+            bool val = firstname.ValidateFirstName(FirstName);
+
+            if (val)
+            {
+                Console.WriteLine("FirstName is Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid FirstName");
+            }
         }
+
+
     }
-}
+    }
